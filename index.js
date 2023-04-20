76ba79747ae50d238d6d6c19171a7f5da59444ce
Samuel <sallenellas@gmail.com>
2023-04-20 03:19:26 -0500

    Beginning code

---
+++ index.js
@@ -0,0 +1,7 @@
+let square = document.querySelector('squares');
+const button = document.getElementById('generate');
+
+button.addEventListener('click', (e) => {
+    console.log('CLICKED');
+    e.preventDefault();
+});
